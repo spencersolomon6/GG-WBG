21b527c10e33d5186e3b11c6ef98d35f81ff8d6a
Evan Ordona <evanordona@hotmail.com>
2023-01-03 03:40:51 -0500

    formatting and comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
+// connects to backend server io
 const socket = io.connect("http://localhost:4000");
 
 function App() {
@@ -13,16 +14,19 @@ function App() {
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
 
+  // Runs when join room button is clicked. Calls join_room in backend, passing the room.
   const joinRoom = () => {
     if (room !== "") {
       socket.emit("join_room", room);
     }
   };
 
+  // Runs when send message button is clicked. Calls send_message in backend, passing the message and room.
   const sendMessage = () => {
     socket.emit("send_message", { message, room });
   };
 
+  // Listens for a receive_message call from backend. Updates messageRecieved with the message. 
   useEffect(() => {
     socket.on("recieve_message", (data) => {
       setMessageReceived(data.message);
@@ -32,7 +36,9 @@ function App() {
 
   return (
     <div className="App">
+
       <h1>Web Based Game</h1>
+
       <input
         placeholder='Room Number'
         onChange={(event) => {
@@ -41,17 +47,21 @@ function App() {
       />
 
       <button onClick={joinRoom}>Join Room</button>
-      <br />
-      <br />
+
+      <br /><br />
+
       <input
         placeholder='Message'
-        onChange={(event => {
+        onChange={(event) => {
           setMessage(event.target.value);
-        })}
+        }}
       />
+
       <button onClick={sendMessage}>Send Message</button>
+
       <h1>Message: </h1>
       {'Message : ' + messageReceived}
+
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { Server } = require('socket.io');
-const app = express();
-const portNumber = 4000;
 const http = require('http');
 const cors = require('cors');
 
+// express setup
+const app = express();
+const portNumber = process.env.PORT || 4000;
+
 app.use(cors());
 
+// socket.io setup
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -16,21 +19,27 @@ const io = new Server(server, {
     }
 });
 
+// Makes a connection to socket.io and listens for emits
 io.on('connection', (socket) => {
+
     console.log(`User Connected: ${socket.id}`)
 
+    // Listens for emit and then joins the room the user inputed
     socket.on('join_room', (data) => {
         console.log('Joining Room: ' + data)
         socket.join(data);
     });
 
+    // Sends a message to the room the user
     socket.on("send_message", (data) => {
         console.log('Message Received in server: ' + data.message);
+
+        // Emits a message to user listening
         socket.to(data.room).emit('receive_message', data);
     })
 });
 
-
+// Starts up server on portNumber
 server.listen(portNumber, () => {
     console.log('Server listinging listening on localhost:' + portNumber + '...');
 });
